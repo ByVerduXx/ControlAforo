b2cf1b8f792b05a16dd7318b59de3336a9d6892f
jmanufa01 <jose.fando@edu.uah.es>
2022-01-02 17:24:58 +0100

    Inicio NotificacionesCovid

---
+++ PECL2/NotificacionesCovid/app/src/main/java/com/example/notificacionescovid/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.notificacionescovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.notificacionescovid.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+
+    }
+}

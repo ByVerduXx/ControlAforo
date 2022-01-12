46ea054dffdb3a58ce19797d0a0c7cefc452de6e
jmanufa01 <jose.fando@edu.uah.es>
2022-01-12 01:26:25 +0100

    Conexion Mqtt Finalizada

--- PECL2/NotificacionesCovid/app/src/main/java/com/example/notificacionescovid/Mqtt/ServicioMqtt.java
+++ PECL2/NotificacionesCovid/app/src/main/java/com/example/notificacionescovid/Mqtt/ServicioMqtt.java
@@ -4,6 +4,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
@@ -20,7 +21,7 @@ import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.*;
 
 
-public class ServicioMqtt extends org.eclipse.paho.android.service.MqttService {
+public class ServicioMqtt extends Service{
 
 
 

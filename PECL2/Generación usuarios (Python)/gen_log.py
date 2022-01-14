429aa04b3195541a35503c9eb9baf94af04edf04
Planplo <74658720+Planplo@users.noreply.github.com>
2022-01-14 10:48:12 +0100

    arreglado el fallo que generaba horas de entradas muy tardías

--- PECL2/Generación usuarios (Python)/gen_log.py
+++ PECL2/Generación usuarios (Python)/gen_log.py
@@ -3,16 +3,16 @@ import datetime
 
 
 def gen_fecha():
-    f = open ("log.sql","w")
+    f = open ("log.txt","a")
     f.write("INSERT INTO log (id_usuario, entrada, salida) VALUES \n")
     for i in range (10,13):
         if i == 10 or i == 12:
             for j in range (1,32):
                 for k in range (1,13):
-                    hora_entrada = datetime.datetime(2021,i,j,random.randint(9,20),random.randint(0,59),random.randint(0,59))
-                    hora_salida = hora_entrada + datetime.timedelta(hours=random.randint(1,8))
+                    hora_entrada = datetime.datetime(2021,i,j,random.randint(9,13),random.randint(0,59),random.randint(0,59))
+                    hora_salida = hora_entrada + datetime.timedelta(hours=random.randint(4,8))
                     while hora_salida.hour>21 or hora_salida.day != hora_entrada.day:
-                        hora_salida = hora_entrada + datetime.timedelta(hours=random.randint(1,8))
+                        hora_salida = hora_entrada + datetime.timedelta(hours=random.randint(4,8))
                     f.write("(")
                     f.write("'")    
                     f.write(str(k))
@@ -31,10 +31,10 @@ def gen_fecha():
         else:
             for j in range(1,31):
                 for k in range (1,13):
-                    hora_entrada = datetime.datetime(2021,i,j,random.randint(9,20),random.randint(0,59),random.randint(0,59))
-                    hora_salida = hora_entrada + datetime.timedelta(hours=random.randint(1,8))
+                    hora_entrada = datetime.datetime(2021,i,j,random.randint(9,13),random.randint(0,59),random.randint(0,59))
+                    hora_salida = hora_entrada + datetime.timedelta(hours=random.randint(4,8))
                     while hora_salida.hour>21 or hora_salida.day != hora_entrada.day:
-                        hora_salida = hora_entrada + datetime.timedelta(hours=random.randint(1,8))
+                        hora_salida = hora_entrada + datetime.timedelta(hours=random.randint(4,8))
                     f.write("(")
                     f.write("'")    
                     f.write(str(k))
@@ -47,7 +47,7 @@ def gen_fecha():
                     f.write("'")
                     f.write(str(hora_salida))
                     f.write("'")
-                    f.write("),")
+                    f.write(")")
                     f.write("\n")
     f.write(";")
     f.close()
